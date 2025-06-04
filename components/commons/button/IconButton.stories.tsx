b25be5e6a710965ccd70f38f1c777a4d378a4226
kimdongyoung <wwweric12@naver.com>
2025-06-04 20:52:46 +0900

    design: IconButton 구현 #3 - IconButton에 필요한 svg 추가 - 필요없는 파일 삭제

---
+++ components/commons/button/IconButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import IconButton from "./IconButton";
+
+const meta: Meta<typeof IconButton> = {
+  title: "Components/IconButton",
+  component: IconButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const IconBtn: Story = {
+  args: {
+    type: "button",
+    text: "button",
+    sort: "email",
+  },
+};
---
+++ components/commons/button/IconButton.tsx
@@ -0,0 +1,29 @@
+export interface IconButtonProps {
+  type?: "button" | "submit";
+  sort: "email" | "naver" | "kakao";
+  text: string;
+  onButtonClick: () => void;
+}
+
+const IconButton = ({ type, sort, text, onButtonClick }: IconButtonProps) => {
+  const IconVariants = {
+    email: "bg-primary-darkGray text-white",
+    kakao: "bg-[#FACF44] text-primary-mainText",
+    naver: "bg-[#06BE34] text-white ",
+  };
+
+  return (
+    <button
+      className={` ${IconVariants[sort]} flex h-[48px] w-full max-w-[350px] items-center gap-[4px] rounded-md px-[16px] py-[13px]`}
+      type={type}
+      onClick={onButtonClick}
+    >
+      {sort === "email" && <img src="icons/email-icon.svg" alt="email" />}
+      {sort === "kakao" && <img src="icons/kakao-icon.svg" alt="kakao" />}
+      {sort === "naver" && <img src="icons/naver-icon.svg" alt="naver" />}
+      <div className="body1 w-full text-center">{text}</div>
+    </button>
+  );
+};
+
+export default IconButton;
