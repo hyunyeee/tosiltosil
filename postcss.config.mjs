b772cf5ffd7418fb45e43ead2ddfa528ad5f3d1c
kimdongyoung <wwweric12@naver.com>
2025-06-06 16:38:51 +0900

    chore: postcss.config.mjs tailwind v4에 맞게 변경 #3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-// postcss.config.mjs
-/** @type {import('postcss').Config} */
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
