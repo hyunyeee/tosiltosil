b737e86719f727ddc3d6931f46994162769eecc0
hyunyeee <5004ohy@naver.com>
2025-06-08 02:57:35 +0900

    feat: 인증번호 유효시간 표시 기능 구현 #4

--- components/auth/input/CodeInput.tsx
+++ components/auth/input/CodeInput.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import InputWrapper from "@/components/auth/input/InputWrapper";
 
 interface CodeInputProps {
@@ -13,13 +16,30 @@ const CodeInput = ({
   value,
   onInputChange,
 }: CodeInputProps) => {
+  const [timeLeft, setTimeLeft] = useState(300); // 5분
+
+  const formatTime = (seconds: number) => {
+    const min = String(Math.floor(seconds / 60)).padStart(1, "0");
+    const sec = String(seconds % 60).padStart(2, "0");
+    return `${min}:${sec}`;
+  };
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onInputChange(name, e.target.value);
   };
 
-  const handleClearClick = () => {
+  const handleResendCode = () => {
     onInputChange(name, "");
+    setTimeLeft(300);
   };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <InputWrapper
@@ -33,11 +53,11 @@ const CodeInput = ({
         value={value}
         onChange={handleInputChange}
       />
-      <p className="caption2">5:00</p>
+      <p className="caption2">{formatTime(timeLeft)}</p>
       <button
         type="button"
         className="cursor-pointer"
-        onClick={handleClearClick}
+        onClick={handleResendCode}
       >
         <img
           src="/icons/re-send-icon.svg"
