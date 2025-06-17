9fa5e4b0fdacc0aab897fd307f88e477c8d4e326
hyunyeee <5004ohy@naver.com>
2025-06-17 19:38:11 +0900

    design: Auth 레이아웃 지정 #10
    - 로그인, 회원가입, 비밀번호 찾기, 비밀번호 재설정 페이지

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import PolicyFooter from "@/components/commons/footer/PolicyFooter";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex h-full flex-col justify-between px-[20px] pb-[26px]">
+      {children}
+      <PolicyFooter />
+    </div>
+  );
+}
