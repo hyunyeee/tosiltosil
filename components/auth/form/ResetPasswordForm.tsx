8530bf30be1cbdce41d8de199c5c5ffebb123511
hyunyeee <5004ohy@naver.com>
2025-06-17 19:51:18 +0900

    feat: 비밀번호 초기화 Form 구현 #11

---
+++ components/auth/form/ResetPasswordForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import PrimaryButton from "@/components/commons/button/PrimaryButton";
+import PasswordInput from "@/components/auth/input/PasswordInput";
+import { AUTH_ERROR_MESSAGE } from "@/constants/authErrorMessage";
+
+const ResetPasswordForm = () => {
+  const [formData, setFormData] = useState({
+    password: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    password: "",
+  });
+
+  const handleInputChange = (name: string, value: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    if (name === "password") {
+      const isValidPassword =
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(value);
+      setFormErrors((prev) => ({
+        ...prev,
+        PASSWORD: isValidPassword ? "" : AUTH_ERROR_MESSAGE.PASSWORD,
+      }));
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log("비밀번호 변경 Form:", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-[60px]">
+        <h1 className="title2 mt-[163px] text-center">비밀번호 재설정</h1>
+        <div className="mb-[43px] flex flex-col gap-[27px]">
+          <PasswordInput
+            name="password"
+            value={formData.password}
+            errorMessage={formErrors.password}
+            onInputChange={handleInputChange}
+          />
+          <PasswordInput
+            name="password"
+            value={formData.password}
+            errorMessage={formErrors.password}
+            onInputChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <PrimaryButton
+        size="main"
+        text="로그인 페이지로"
+        isActive={false}
+        onButtonClick={() => {}}
+      />
+    </form>
+  );
+};
+
+export default ResetPasswordForm;
