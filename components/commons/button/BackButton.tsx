cd6634dba37a3e93a2ebd0b46fb7cc40dd7de7fa
kimdongyoung <wwweric12@naver.com>
2025-06-19 16:12:46 +0900

    design: BackButton ui 구현 #9

---
+++ components/commons/button/BackButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export interface BackButtonProps {
+  onBackClick: () => void;
+}
+
+const BackButton = ({ onBackClick }: BackButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onBackClick}
+      className="flex w-fit gap-[6px]"
+    >
+      <img src="icons/prev-icon.svg" alt="뒤로가기" />
+      <span className="footout text-[#33383c]">뒤로가기</span>
+    </button>
+  );
+};
+
+export default BackButton;
