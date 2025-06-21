2fa5a7ff7f7dd47299f5bfefcf0926d1ceba0fc5
hyunyeee <5004ohy@naver.com>
2025-06-21 11:58:48 +0900

    refactor: EmailInputProps 타입 수정 #18

--- components/auth/input/EmailInput.tsx
+++ components/auth/input/EmailInput.tsx
@@ -2,8 +2,8 @@ import InputWrapper from "@/components/auth/input/InputWrapper";
 import { AUTH_ERROR_MESSAGE } from "@/constants/authErrorMessage";
 
 interface EmailInputProps {
-  name: string; //name: keyof typeof formData;
-  errorMessage: string;
+  name: "email";
+  errorMessage?: string;
   value: string;
   onInputChange: (name: EmailInputProps["name"], value: string) => void;
 }
