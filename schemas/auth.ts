2218505243ccf461992e4c1bed74833e405ad90a
kimdongyoung <wwweric12@naver.com>
2025-06-23 19:12:11 +0900

    feat: resetPasswordSchema 추가 및 SignupFormData type 변경#21

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -30,10 +30,7 @@ export const signupSchema = z
       }),
   })
   .extend({
-    confirmPassword: z.string({
-      required_error:
-        "영문,숫자,특수문자를 포함하여 8글자 이상으로 입력해주세요",
-    }),
+    confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "입력하신 비밀번호와 일치하지 않습니다.",
@@ -41,20 +38,30 @@ export const signupSchema = z
   });
 
 export const requestCodeSchema = z.object({
-  email: z
-    .string({ required_error: "이메일을 입력해주세요" })
-    .email("유효한 이메일 형식이 아닙니다"),
+  email: z.string().email("유효한 이메일 형식이 아닙니다"),
 });
 
 export const verifyCodeSchema = z.object({
-  code: z
-    .string({ required_error: "인증번호를 입력해주세요" })
-    .regex(/^\d{6}$/, "인증번호는 숫자 6자리여야 합니다"),
+  code: z.string().regex(/^\d{6}$/, "인증번호는 숫자 6자리여야 합니다"),
 });
 
-export type VerifyCodeFormData = z.infer<typeof verifyCodeSchema>;
-export type RequestCodeFormData = z.infer<typeof requestCodeSchema>;
+export const resetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        "영문,숫자,특수문자를 포함하여 8글자 이상으로 입력해주세요"
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "입력하신 비밀번호와 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
+
 export type LoginFormData = z.infer<typeof loginSchema>;
-export type SignupFormData = z.infer<typeof signupSchema> & {
-  confirmPassword: string;
-};
+export type SignupFormData = z.infer<typeof signupSchema>;
+export type RequestCodeFormData = z.infer<typeof requestCodeSchema>;
+export type VerifyCodeFormData = z.infer<typeof verifyCodeSchema>;
+export type ResetPasswordFormData = z.infer<typeof resetPasswordSchema>;
