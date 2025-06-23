86f7f2822577472336c477991d43d8f46e6be286
kimdongyoung <wwweric12@naver.com>
2025-06-23 18:25:37 +0900

    refactor: 비밀번호 찾기 page 경로 수정 #21

--- app/(auth)/find-password/reset/page.tsx
+++ app/(auth)/find-password/reset/page.tsx
@@ -1,9 +1,5 @@
 import ResetPasswordForm from "@/components/auth/form/ResetPasswordForm";
 
 export default function ResetPasswordPage() {
-  return (
-    <div>
-      <ResetPasswordForm />
-    </div>
-  );
+  return <ResetPasswordForm />;
 }
