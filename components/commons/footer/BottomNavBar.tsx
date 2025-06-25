a71364c645836d6039dc4f844944873674593ea0
hyunyeee <5004ohy@naver.com>
2025-06-25 21:33:26 +0900

    fix: usePathname 항상 호출, 값만 조건 처리하도록 수정 #17

--- components/commons/footer/BottomNavBar.tsx
+++ components/commons/footer/BottomNavBar.tsx
@@ -5,34 +5,8 @@ import { usePathname } from "next/navigation";
 
 // 스토리북에서 강제로 pathname 전달할 수 있도록 props 추가
 const BottomNavBar = ({ mockPathname }: { mockPathname?: string }) => {
-  const pathname = mockPathname || usePathname();
-
-  const navItems = [
-    {
-      label: "홈",
-      href: "/home",
-      icon: "/icons/nav-home-icon-default.svg",
-      activeIcon: "/icons/nav-home-icon-current.svg",
-    },
-    {
-      label: "검색",
-      href: "/friend/search",
-      icon: "/icons/nav-search-icon-default.svg",
-      activeIcon: "/icons/nav-search-icon-current.svg",
-    },
-    {
-      label: "캘린더",
-      href: "/calendar",
-      icon: "/icons/nav-calendar-icon-default.svg",
-      activeIcon: "/icons/nav-calendar-icon-current.svg",
-    },
-    {
-      label: "친구목록",
-      href: "/friend/list",
-      icon: "/icons/nav-friend-list-icon-default.svg",
-      activeIcon: "/icons/nav-friend-list-icon-current.svg",
-    },
-  ];
+  const realPathname = usePathname();
+  const pathname = mockPathname ?? realPathname;
 
   return (
     <nav className="fixed bottom-0 z-10 flex w-full max-w-[430px] items-center justify-around border-t border-black/30 bg-white py-[10px]">
