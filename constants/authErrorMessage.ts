bf9c0ae3c785d48d67ef0acaf65e7f30d9934f42
hyunyeee <5004ohy@naver.com>
2025-06-25 16:29:07 +0900

    feat: 닉네임 에러 메세지 추가 #15

--- constants/authErrorMessage.ts
+++ constants/authErrorMessage.ts
@@ -3,4 +3,5 @@ export const AUTH_ERROR_MESSAGE = {
   CODE: "인증번호 6글자를 입력해주세요.",
   PASSWORD: "영문,숫자,특수문자를 포함하여 8글자 이상으로 입력해주세요.",
   CONFIRM_PASSWORD: "입력하신 비밀번호와 맞지 않습니다. 다시 입력해주세요.",
+  NICKNAME: "2글자 이상 8글자 이하로 입력해주세요.",
 };
