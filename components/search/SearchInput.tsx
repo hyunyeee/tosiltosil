4a2d74c08605bf86931b3894558f5b81152ff77e
hyunyeee <5004ohy@naver.com>
2025-06-25 17:49:17 +0900

    design: 검색창 컴포넌트 UI 구현 #15

---
+++ components/search/SearchInput.tsx
@@ -0,0 +1,13 @@
+const SearchInput = () => {
+  return (
+    <form className="bg-gray-card border-primary-gray flex items-center gap-2 rounded-[6px] border-1 p-[12px]">
+      <img src="/icons/search-icon.svg" />
+      <input
+        className="w-full focus:ring-0 focus:outline-none"
+        placeholder="닉네임이나 코드를 입력해주세요"
+      />
+    </form>
+  );
+};
+
+export default SearchInput;
