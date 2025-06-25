d9d57d135be1dd55b74f0679fd3ba64de7ff15b8
hyunyeee <5004ohy@naver.com>
2025-06-25 19:44:31 +0900

    design: 친구 검색 페이지 UI 구현 #16
    - 더미데이터 적용

---
+++ app/search/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import SearchInput from "@/components/search/SearchInput";
+import SearchTitle from "@/components/search/SearchTitle";
+import SearchResultList from "@/components/search/SearchResultList";
+import { SearchResult } from "@/types/search";
+
+export default function SearchPage() {
+  const searchResultList: SearchResult[] = [
+    { name: "김하늘", friendCode: 100001, status: "친구" },
+    { name: "박지민", friendCode: 100002, status: "대기중" },
+    { name: "이수연", friendCode: 100003 },
+    { name: "최준호", friendCode: 100004, status: "친구" },
+    { name: "정예린", friendCode: 100005, status: "대기중" },
+    { name: "한세린", friendCode: 100006 },
+    { name: "윤재현", friendCode: 100007, status: "친구" },
+    { name: "서지훈", friendCode: 100008, status: "대기중" },
+    { name: "배은호", friendCode: 100009, status: "친구" },
+    { name: "오지민", friendCode: 100010 },
+    { name: "노지우", friendCode: 100011, status: "친구" },
+    { name: "조은별", friendCode: 100012 },
+    { name: "남지훈", friendCode: 100013, status: "대기중" },
+    { name: "황서윤", friendCode: 100014, status: "친구" },
+    { name: "강민석", friendCode: 100015 },
+  ];
+
+  return (
+    <div className="mt-[28px]">
+      <div className="px-[20px]">
+        <SearchInput />
+        <SearchTitle />
+      </div>
+      <SearchResultList searchResultList={searchResultList} />
+    </div>
+  );
+}
