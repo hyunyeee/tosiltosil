07d07d83686821a59a9e2115a22354a97a639b4f
hyunyeee <5004ohy@naver.com>
2025-06-26 20:25:53 +0900

    refactor: 친구 검색 결과 mock 파일 분리 #16

---
+++ app/mocks/searchResults.ts
@@ -0,0 +1,84 @@
+import { SearchResult } from "@/types/search";
+
+export const searchResultList: SearchResult[] = [
+  {
+    id: "1",
+    nickname: "김하늘",
+    profileImg: "",
+    code: "100001",
+    relationship: "FRIEND",
+  },
+  {
+    id: "2",
+    nickname: "박지민",
+    profileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+    code: "100002",
+    relationship: "PENDING",
+  },
+  {
+    id: "3",
+    nickname: "이수연",
+    profileImg: "",
+    code: "100003",
+  },
+  {
+    id: "4",
+    nickname: "최준호",
+    profileImg: "https://randomuser.me/api/portraits/men/43.jpg",
+    code: "100004",
+    relationship: "FRIEND",
+  },
+  {
+    id: "5",
+    nickname: "정예린",
+    profileImg: "https://randomuser.me/api/portraits/women/55.jpg",
+    code: "100005",
+    relationship: "PENDING",
+  },
+  {
+    id: "6",
+    nickname: "한세린",
+    profileImg: "",
+    code: "100006",
+  },
+  {
+    id: "7",
+    nickname: "윤재현",
+    profileImg: "https://randomuser.me/api/portraits/men/37.jpg",
+    code: "100007",
+    relationship: "FRIEND",
+  },
+  {
+    id: "8",
+    nickname: "서지훈",
+    profileImg: "",
+    code: "100008",
+    relationship: "PENDING",
+  },
+  {
+    id: "9",
+    nickname: "배은호",
+    profileImg: "https://randomuser.me/api/portraits/women/7.jpg",
+    code: "100009",
+    relationship: "FRIEND",
+  },
+  {
+    id: "10",
+    nickname: "오지민",
+    profileImg: "",
+    code: "100010",
+  },
+  {
+    id: "11",
+    nickname: "노지우",
+    profileImg: "https://randomuser.me/api/portraits/men/15.jpg",
+    code: "100011",
+    relationship: "FRIEND",
+  },
+  {
+    id: "12",
+    nickname: "조은별",
+    profileImg: "https://randomuser.me/api/portraits/women/48.jpg",
+    code: "100012",
+  },
+];
