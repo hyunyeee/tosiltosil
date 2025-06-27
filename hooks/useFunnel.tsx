2f4074acf85b77c9ae4875d47ca3f7dfc0a1c370
kimdongyoung <wwweric12@naver.com>
2025-06-27 14:59:04 +0900

    feat: useFunnel 구현 #19 - 라우터를 사용하지 않고 내부 상태로만 관리

---
+++ hooks/useFunnel.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { ReactElement, ReactNode, useState } from "react";
+
+type NonEmptyTuple<T> = readonly [T, ...T[]];
+
+interface StepProps<StepKey extends string> {
+  name: StepKey;
+  children: ReactNode;
+}
+interface FunnelProps<StepKey extends string> {
+  children: ReactElement<StepProps<StepKey>>[];
+}
+
+export default function useFunnel<Steps extends NonEmptyTuple<string>>(
+  steps: Steps
+) {
+  type StepKey = Steps[number];
+
+  const [currentStep, setCurrentStep] = useState<StepKey>(steps[0]);
+
+  const idx = steps.indexOf(currentStep);
+  const isFirst = idx === 0;
+  const isLast = idx === steps.length - 1;
+
+  const nextStep = () => {
+    if (!isLast) setCurrentStep(steps[idx + 1]);
+  };
+
+  const prevStep = () => {
+    if (!isFirst) setCurrentStep(steps[idx - 1]);
+  };
+
+  const Funnel = ({ children }: FunnelProps<StepKey>) => {
+    const targetStep = children.find(
+      (childStep) => childStep.props.name === currentStep
+    );
+    return <>{targetStep}</>;
+  };
+
+  const Step = (props: StepProps<StepKey>) => {
+    return <>{props.children}</>;
+  };
+
+  return {
+    currentStep,
+    isFirst,
+    isLast,
+    nextStep,
+    prevStep,
+    Funnel,
+    Step,
+  } as const;
+}
