dcf9feed7446ac55137c2e3c7d9287243be758db
kimdongyoung <wwweric12@naver.com>
2025-06-30 21:18:27 +0900

    refactor: 뒤로가기 header props에 따라 분기 처리 #19

--- components/commons/header/BackHeader.tsx
+++ components/commons/header/BackHeader.tsx
@@ -1,35 +1,38 @@
 "use client";
 
+import Link from "next/link";
 import BackButton from "../button/BackButton";
 
 interface BackHeaderProps {
   onBackClick: () => void;
-  onAlarmClick: () => void;
-  onSettingClick: () => void;
-  hasNotification: boolean;
+  onAlarmClick?: () => void;
+  showActions?: boolean;
+  hasNotification?: boolean;
 }
 
 const BackHeader = ({
   onBackClick,
   onAlarmClick,
-  onSettingClick,
-  hasNotification,
+  hasNotification = false,
+  showActions = false,
 }: BackHeaderProps) => {
   const alarmIconSrc = hasNotification
     ? "/icons/alarm-active-icon.svg"
     : "/icons/alarm-default-icon.svg";
 
   return (
-    <div className="flex w-full items-center justify-between">
+    <div className="flex w-full items-center justify-between py-[12px]">
       <BackButton onBackClick={onBackClick} />
-      <div className="flex gap-[5px]">
-        <button onClick={onAlarmClick}>
-          <img src={alarmIconSrc} alt="알림창" />
-        </button>
-        <button onClick={onSettingClick}>
-          <img src="/icons/set-icon.svg" alt="설정 및 마이페이지" />
-        </button>
-      </div>
+      {showActions && (
+        <div className="flex gap-2">
+          <button onClick={onAlarmClick}>
+            <img src={alarmIconSrc} alt="알림창" />
+          </button>
+          <Link href="/mypage">
+            <img src="/icons/set-icon.svg" alt="설정 및 마이페이지" />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
