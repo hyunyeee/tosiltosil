52cd862bc77c2322b32fe6322fa3793dae0e01ae
hyunyeee <5004ohy@naver.com>
2025-07-02 18:59:39 +0900

    design: 불필요한 div 제거 #26

--- components/search/SearchResultList.tsx
+++ components/search/SearchResultList.tsx
@@ -9,13 +9,9 @@ const SearchResultList = ({ searchResultList }: SearchResultListProps) => {
   return (
     <div className="bg-gray-card h-full min-h-[calc(100vh-146px)]">
       {searchResultList.length === 0 ? (
-        <div className="">
-          <div className="flex h-full flex-col items-center justify-center gap-[12px]">
-            <img className="mt-[214px]" src="/images/result-empty.svg" />
-            <p className="subhead2 text-primary-darkGray">
-              검색결과가 없습니다.
-            </p>
-          </div>
+        <div className="flex h-full flex-col items-center justify-center gap-[12px]">
+          <img className="mt-[214px]" src="/images/result-empty.svg" />
+          <p className="subhead2 text-primary-darkGray">검색결과가 없습니다.</p>
         </div>
       ) : (
         <div className="flex flex-col gap-[13px] p-[20px]">
