a9f4d37cc6e0ef4c50d237e8f80933b75260b63e
hyunyeee <5004ohy@naver.com>
2025-07-03 14:50:05 +0900

    refactor: 검색 결과 리스트 컴포넌트 구현 #26

---
+++ components/friend/search/SearchResultList.tsx
@@ -0,0 +1,28 @@
+import FriendListFrame from "@/components/friend/FriendListFrame";
+import SearchResultCard from "@/components/friend/search/SearchResultCard";
+import { SearchResult } from "@/types/friend";
+
+interface SearchResultListProps {
+  searchResultList: SearchResult[];
+}
+
+const SearchResultList = ({ searchResultList }: SearchResultListProps) => {
+  return (
+    <FriendListFrame isEmpty={searchResultList.length === 0}>
+      <div className="flex flex-col gap-[13px] p-[20px]">
+        {searchResultList.map(
+          ({ nickname, profileImg, code, relationship }) => (
+            <SearchResultCard
+              key={code}
+              nickname={nickname}
+              profileImg={profileImg}
+              code={code}
+              relationship={relationship}
+            />
+          )
+        )}
+      </div>
+    </FriendListFrame>
+  );
+};
+export default SearchResultList;
