f5bacfd16b39274ede04d74ee61810b16d7e1301
hyunyeee <5004ohy@naver.com>
2025-07-04 21:06:04 +0900

    feat: 친구 요청 페이지 구현 #26

---
+++ app/friend/pending/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useDebounce";
+import SearchInput from "@/components/friend/search/SearchInput";
+import BasicTitle from "@/components/friend/BasicTitle";
+import PendingList from "@/components/friend/pending/PendingList";
+import { friendResultList } from "@/app/mocks/searchResults";
+
+export default function FriendPendingPage() {
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 500);
+
+  const handleChangeQuery = (value: string) => {
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    if (debouncedQuery.trim()) {
+      console.log("검색어:", debouncedQuery);
+      // TODO: 실제 API 요청
+    }
+  }, [debouncedQuery]);
+
+  return (
+    <div className="mt-[28px]">
+      <div className="px-[20px]">
+        <SearchInput value={query} onInputChange={handleChangeQuery} />
+        <BasicTitle iconSrc="/icons/friend-request-icon.svg" text="친구요청" />
+      </div>
+      <PendingList friendRequestList={friendResultList} />
+    </div>
+  );
+}
