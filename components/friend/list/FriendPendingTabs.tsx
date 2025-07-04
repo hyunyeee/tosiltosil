fd2026fcd4df6c11ddacad2800378ba6707df7d5
hyunyeee <5004ohy@naver.com>
2025-07-04 19:20:13 +0900

    feat: 친구 요청/수락 관리 페이지로 이동하는 버튼 구현 #26

---
+++ components/friend/list/FriendPendingTabs.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface FriendPendingTabs {
+  requestCount: number;
+  pendingCount: number;
+}
+
+const FriendPendingTabs = ({
+  requestCount,
+  pendingCount,
+}: FriendPendingTabs) => {
+  return (
+    <div className="mt-[22px] flex justify-end gap-[11px]">
+      <Link href="/friend/pending" className="flex items-center gap-[4px]">
+        <b className="footout">친구 요청</b>
+        <b className="caption2">{requestCount}</b>
+        <img src="/icons/move-arrow-icon.svg" />
+      </Link>
+      <Link href="/friend/request" className="flex items-center gap-[4px]">
+        <b className="footout">수락 대기중</b>
+        <b className="caption2">{pendingCount}</b>
+        <img src="/icons/move-arrow-icon.svg" />
+      </Link>
+    </div>
+  );
+};
+
+export default FriendPendingTabs;
