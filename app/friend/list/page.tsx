c8ee6e3038ce8a5f47bcef4fbb1632f33d89c394
hyunyeee <5004ohy@naver.com>
2025-07-04 19:24:12 +0900

    feat: 친구 목록 페이지 구현 #26

---
+++ app/friend/list/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useDebounce";
+import SearchInput from "@/components/friend/search/SearchInput";
+import FriendPendingTabs from "@/components/friend/list/FriendPendingTabs";
+import FriendListTitle from "@/components/friend/list/FriendListTitle";
+import FriendList from "@/components/friend/list/FriendResultList";
+import { friendResultList } from "@/app/mocks/searchResults";
+
+export default function FriendListPage() {
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 500);
+
+  const handleChangeQuery = (value: string) => {
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    if (debouncedQuery.trim()) {
+      console.log("검색어:", debouncedQuery);
+      // TODO: 실제 API 요청
+    }
+  }, [debouncedQuery]);
+
+  return (
+    <div className="mt-[28px]">
+      <div className="px-[20px]">
+        <SearchInput value={query} onInputChange={handleChangeQuery} />
+        <FriendPendingTabs requestCount={3} pendingCount={2} />
+      </div>
+      <FriendListTitle />
+      <FriendList friendResultList={friendResultList} />
+    </div>
+  );
+}
