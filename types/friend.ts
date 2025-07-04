9cf2e8908ef179f2f672dc901cec2fe04ddff764
hyunyeee <5004ohy@naver.com>
2025-07-04 20:38:06 +0900

    refactor: Base 타입 추출 및 확장 #26

--- types/friend.ts
+++ types/friend.ts
@@ -1,14 +1,14 @@
-export interface SearchResult {
+interface BaseUserInfo {
   id?: string;
   nickname: string;
   profileImg: string;
   code: string;
-  relationship?: "FRIEND" | "PENDING";
 }
 
-export interface FriendResult {
-  id?: string;
-  nickname: string;
-  profileImg: string;
-  code: string;
+export interface FriendRequest extends BaseUserInfo {}
+
+export interface FriendResult extends BaseUserInfo {}
+
+export interface SearchResult extends BaseUserInfo {
+  relationship?: "FRIEND" | "PENDING";
 }
