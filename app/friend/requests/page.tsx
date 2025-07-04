aaea9206e444c72c060d2149a3ebc0e1c46ddaf3
hyunyeee <5004ohy@naver.com>
2025-07-04 21:08:13 +0900

    feat: 친구 수락 대기 페이지 구현 #26

---
+++ app/friend/requests/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useDebounce";
+import SearchInput from "@/components/friend/search/SearchInput";
+import BasicTitle from "@/components/friend/BasicTitle";
+import RequestList from "@/components/friend/request/RequestList";
+import { friendResultList } from "@/app/mocks/searchResults";
+
+export default function FriendRequestPage() {
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 500);
+
+  const handleChangeQuery = (value: string) => {
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    if (debouncedQuery.trim()) {
+      console.log("검색어:", debouncedQuery);
+      // TODO: 실제 API 요청
+    }
+  }, [debouncedQuery]);
+
+  return (
+    <div className="mt-[28px]">
+      <div className="px-[20px]">
+        <SearchInput value={query} onInputChange={handleChangeQuery} />
+        <BasicTitle
+          iconSrc="/icons/friend-request-icon.svg"
+          text="수락대기중"
+        />
+      </div>
+      <RequestList friendRequestList={friendResultList} />
+    </div>
+  );
+}
