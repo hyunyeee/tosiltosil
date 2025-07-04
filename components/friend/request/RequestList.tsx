4c43559db63781b396a1f5a4cd6d29af3a88b47f
hyunyeee <5004ohy@naver.com>
2025-07-04 21:07:56 +0900

    design: 친구 수락 대기 목록 RequestList 컴포넌트 구현 #26

---
+++ components/friend/request/RequestList.tsx
@@ -0,0 +1,25 @@
+import FriendListFrame from "@/components/friend/FriendListFrame";
+import RequestCard from "@/components/friend/request/RequestCard";
+import { FriendRequest } from "@/types/friend";
+
+interface FriendRequestListProps {
+  friendRequestList: FriendRequest[];
+}
+
+const RequestList = ({ friendRequestList }: FriendRequestListProps) => {
+  return (
+    <FriendListFrame isEmpty={friendRequestList.length === 0}>
+      <div className="flex flex-col gap-[13px] p-[20px]">
+        {friendRequestList.map(({ nickname, profileImg, code }) => (
+          <RequestCard
+            key={code}
+            nickname={nickname}
+            profileImg={profileImg}
+            code={code}
+          />
+        ))}
+      </div>
+    </FriendListFrame>
+  );
+};
+export default RequestList;
