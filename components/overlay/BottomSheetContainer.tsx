e8b004112581956ee3d1f06775cb54c79ff244d8
hyunyeee <5004ohy@naver.com>
2025-07-06 10:47:50 +0900

    feat: BottomSheetContainer 컴포넌트 구현 #27

---
+++ components/overlay/BottomSheetContainer.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface IBottomSheetContainer {
+  children: ReactNode;
+}
+
+const BottomSheetContainer = ({ children }: IBottomSheetContainer) => {
+  return (
+    <div className="fixed bottom-0 left-1/2 w-full max-w-[430px] -translate-x-1/2 rounded-t-2xl bg-green-400 p-6 shadow-lg">
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default BottomSheetContainer;
