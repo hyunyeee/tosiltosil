117895d8e454585fe5a9aaed433381fefe0aa50a
hyunyeee <5004ohy@naver.com>
2025-07-11 20:47:36 +0900

    feat: 메인페이지 활동중 친구 목록 컴포넌트 구현 #32

---
+++ components/home/FriendStatusBoard.tsx
@@ -0,0 +1,22 @@
+import ProfileCard from "@/components/home/ProfileCard";
+import { friendStatusList } from "@/constants/mocks/FriendStatusList";
+
+const FriendStatusBoard = () => {
+  const activeFriendCount = friendStatusList.length;
+
+  return (
+    <div className="flex flex-col">
+      <h3 className="title3 ml-[19px]">활동 목록</h3>
+      <p className="footnote mr-[16px] ml-auto">
+        현재 활동중인 친구 <b className="footout">{activeFriendCount}</b>명
+      </p>
+      <div className="ml-[20px] flex gap-[8px] overflow-x-scroll pt-[13px] pr-[13px] pb-[24px]">
+        {friendStatusList.map((friend, index) => (
+          <ProfileCard key={friend.memberId} {...friend} isMe={index === 0} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FriendStatusBoard;
