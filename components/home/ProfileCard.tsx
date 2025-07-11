952680c5f739e737f2025aecd30dc21cab5c240c
hyunyeee <5004ohy@naver.com>
2025-07-11 20:34:30 +0900

    feat: 메인페이지 활동중 친구 컴포넌트 구현 #32

---
+++ components/home/ProfileCard.tsx
@@ -0,0 +1,41 @@
+import { formatISODurationToHourMinute } from "@/utils/time";
+
+interface ProfileCard {
+  memberId: string;
+  nickname: string;
+  profileImg: string;
+  totalTime: string;
+  status: "ACTIVE" | "INACTIVE";
+  isMe: boolean;
+}
+const ProfileCard = ({
+  memberId,
+  nickname,
+  profileImg,
+  totalTime,
+  status, // TODO: ACTIVE 인 사용자는 테두리 추가 에정
+  isMe,
+}: ProfileCard) => {
+  return (
+    <div
+      className={`${isMe ? "gap-[6px]" : "gap-[7px]"} flex flex-shrink-0 flex-col items-center`}
+    >
+      <div className="relative h-[70px] w-[70px] overflow-hidden rounded-full bg-black/60">
+        <div className="absolute inset-0 bg-black/40" />
+        <img
+          src={profileImg || "/images/default-profile-image.svg"}
+          className="h-full w-full object-cover"
+          alt="프로필 이미지"
+        />
+        <p className="body2 absolute inset-0 flex items-center justify-center text-white">
+          {formatISODurationToHourMinute(totalTime)}
+        </p>
+      </div>
+
+      <p className={`${isMe ? "caption1" : "caption2"} text-primary-mainText`}>
+        {nickname}
+      </p>
+    </div>
+  );
+};
+export default ProfileCard;
