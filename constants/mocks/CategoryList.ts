3bbfb9a9c26a499a00529b06547b88cc68d7a233
hyunyeee <5004ohy@naver.com>
2025-07-13 22:07:41 +0900

    chore: 카테고리 목록 목업데이터 추가 #32

---
+++ constants/mocks/CategoryList.ts
@@ -0,0 +1,54 @@
+import { Category } from "@/types/Category";
+
+export const CategoryList: Category[] = [
+  {
+    categoryId: 1,
+    title: "매일할 것",
+    color: "pink",
+  },
+  {
+    categoryId: 2,
+    title: "자기 전 할 것",
+    color: "orange",
+  },
+  {
+    categoryId: 3,
+    title: "주말 할 일",
+    color: "yellow",
+  },
+  {
+    categoryId: 4,
+    title: "운동 루틴",
+    color: "green",
+  },
+  {
+    categoryId: 5,
+    title: "건강 습관",
+    color: "mint",
+  },
+  {
+    categoryId: 6,
+    title: "업무 정리",
+    color: "skyblue",
+  },
+  {
+    categoryId: 7,
+    title: "공부 계획",
+    color: "blue",
+  },
+  {
+    categoryId: 9,
+    title: "감정 일기",
+    color: "pink",
+  },
+  {
+    categoryId: 10,
+    title: "식단 관리",
+    color: "coral",
+  },
+  {
+    categoryId: 11,
+    title: "독서 기록",
+    color: "indigo",
+  },
+];
