a2df0cc846eae0f32f51bb6ebc06a9ccfd387db2
hyunyeee <5004ohy@naver.com>
2025-07-13 19:33:37 +0900

    feat: CategoryLabel 컴포넌트 구현 #32

---
+++ components/home/CategoryLabel.tsx
@@ -0,0 +1,40 @@
+interface CategoryLabelProps {
+  categoryId: number;
+  title: string;
+  color: string;
+  selectedCategoryId: number | null;
+  onCategorySelect: (category: number | null) => void;
+}
+
+const CategoryLabel = ({
+  categoryId,
+  title,
+  color,
+  selectedCategoryId,
+  onCategorySelect,
+}: CategoryLabelProps) => {
+  const isSelected = selectedCategoryId === categoryId;
+
+  const handleSelectCategory = () => {
+    onCategorySelect(categoryId);
+  };
+
+  const backgroundColor = `var(--color-sub-${color})`;
+  const borderColor = `var(--color-primary-${color})`;
+
+  return (
+    <div
+      onClick={handleSelectCategory}
+      className="footnote text-primary-mainText flex-shrink-0 cursor-pointer rounded-[2px] border-[1px] px-[12px] py-[4px] text-center"
+      style={{
+        backgroundColor,
+        borderColor,
+        opacity: isSelected ? 1 : 0.5,
+      }}
+    >
+      {title}
+    </div>
+  );
+};
+
+export default CategoryLabel;
