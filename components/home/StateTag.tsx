ad15b1014be69121217e28060a6b91de04034af9
hyunyeee <5004ohy@naver.com>
2025-07-15 14:48:50 +0900

    refactor: StateTag 스타일 정의 객체 파일 분리 #32

--- components/home/StateTag.tsx
+++ components/home/StateTag.tsx
@@ -1,32 +1,4 @@
-interface StateTagProps {
-  state: "진행전" | "진행중" | "완료" | "실패";
-}
-
-const STATE_STYLE_MAP: Record<
-  StateTagProps["state"],
-  { bg: string; text: string; border: string }
-> = {
-  진행전: {
-    bg: "",
-    text: "text-primary-darkGray",
-    border: "border-[1px] border-primary-darkGray",
-  },
-  진행중: {
-    bg: "",
-    text: "text-primary-mainText",
-    border: "border-[1px] border-primary-mainText",
-  },
-  완료: {
-    bg: "bg-black/70",
-    text: "text-white",
-    border: "",
-  },
-  실패: {
-    bg: "",
-    text: "text-black/70",
-    border: "border-[1px] border-black/70",
-  },
-};
+import { StateTagProps, STATE_STYLE_MAP } from "@/constants/stateTagMap";
 
 const StateTag = ({ state }: StateTagProps) => {
   const { bg, text, border } = STATE_STYLE_MAP[state];
---
+++ constants/stateTagMap.ts
@@ -0,0 +1,29 @@
+export interface StateTagProps {
+  state: "진행전" | "진행중" | "완료" | "실패";
+}
+
+export const STATE_STYLE_MAP: Record<
+  StateTagProps["state"],
+  { bg: string; text: string; border: string }
+> = {
+  진행전: {
+    bg: "",
+    text: "text-primary-darkGray",
+    border: "border-[1px] border-primary-darkGray",
+  },
+  진행중: {
+    bg: "",
+    text: "text-primary-mainText",
+    border: "border-[1px] border-primary-mainText",
+  },
+  완료: {
+    bg: "bg-black/70",
+    text: "text-white",
+    border: "",
+  },
+  실패: {
+    bg: "",
+    text: "text-black/70",
+    border: "border-[1px] border-black/70",
+  },
+};
