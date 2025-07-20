5f415ab4f56f7665054209e3243b4ee800add509
kimdongyoung <wwweric12@naver.com>
2025-07-20 14:18:29 +0900

    refactor: BasicTitle에 버튼 까지 추가 #34 - 추가하는 버튼을 사용하는 컴포넌트도 있어서 수정

--- components/friend/BasicTitle.tsx
+++ components/friend/BasicTitle.tsx
@@ -1,13 +1,30 @@
 interface TitleProps {
-  iconSrc: string;
+  titleIconSrc: string;
   text: string;
+  buttonIconType?: "plus";
 }
 
-const BasicTitle = ({ iconSrc, text }: TitleProps) => {
+type ButtonIconType = NonNullable<TitleProps["buttonIconType"]>;
+
+const BUTTON_ICON_SRC: Record<ButtonIconType, string> = {
+  plus: "/icons/plus-icon.svg",
+};
+
+const BasicTitle = ({ titleIconSrc, text, buttonIconType }: TitleProps) => {
   return (
-    <div className="mt-[21px] mb-[17px] flex items-center gap-[6px] border-b-1 border-black/10 pb-[5px]">
-      <img src={iconSrc} alt="제목 아이콘" />
-      <h2 className="subhead2">{text}</h2>
+    <div className="mt-[21px] mb-[17px] flex w-full items-center justify-between border-b border-black/10 pb-[5px]">
+      <div className="flex items-center gap-[6px]">
+        <img src={titleIconSrc} alt="제목 아이콘" />
+        <h2 className="subhead2">{text}</h2>
+      </div>
+      {buttonIconType && (
+        <button type="button">
+          <img
+            src={BUTTON_ICON_SRC[buttonIconType]}
+            alt={`${buttonIconType}-icon`}
+          />
+        </button>
+      )}
     </div>
   );
 };
