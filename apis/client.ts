2d695c4b4bf071ceb2fa69ad63ab71fc5c81059a
kimdongyoung <wwweric12@naver.com>
2025-07-21 10:42:31 +0900

    refactor: baseUrl 환경변수 검증 로직 추가 #31

--- apis/client.ts
+++ apis/client.ts
@@ -1,5 +1,7 @@
+import { getRequiredEnv } from "@/utils/env";
 import { fetchClient } from "./fetchClient";
 
+const baseURL = getRequiredEnv("NEXT_PUBLIC_API_BASE_URL");
 export const apiClient = fetchClient({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL!,
+  baseURL,
 });
--- apis/server.ts
+++ apis/server.ts
@@ -1,9 +1,11 @@
 import { cookies } from "next/headers";
 import { fetchClient } from "./fetchClient";
+import { getRequiredEnv } from "@/utils/env";
 
 export const serverApiClient = () => {
+  const baseURL = getRequiredEnv("API_BASE_URL");
   return fetchClient({
-    baseURL: process.env.API_BASE_URL!,
+    baseURL,
     defaultHeaders: {
       Cookie: cookies().toString(),
     },
