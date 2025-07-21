4a316b9173f8d268bc4ba7a6b950be1ee8d456d4
kimdongyoung <wwweric12@naver.com>
2025-07-21 10:33:06 +0900

    feat: Drag and Drop 및 category list 구현 #34

---
+++ constants/mocks/CategoryList.ts
@@ -0,0 +1,7 @@
+export const INITIAL_CATEGORY = [
+  { title: "이마트 가서 장보고 오기🐼", color: "red", id: "1" },
+  { title: "스터디 자료 정리하기📚", color: "blue", id: "2" },
+  { title: "운동하기🏃‍♂️", color: "purple", id: "4" },
+  { title: "운동하기🏃‍♂️", color: "yellow", id: "5" },
+  { title: "운asd동하기🏃‍♂️", color: "pink", id: "6" },
+];
