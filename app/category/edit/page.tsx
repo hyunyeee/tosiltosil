06538f6e444d4515391570e12a3effd2a6ef30bd
kimdongyoung <wwweric12@naver.com>
2025-07-24 09:32:49 +0900

    design: padding 및 min-h 수정 #34

--- app/category/edit/page.tsx
+++ app/category/edit/page.tsx
@@ -6,7 +6,7 @@ import { INITIAL_CATEGORY } from "@/constants/mocks/CategoryList";
 export default function CategoryEditPage() {
   // TODO: 카테고리 받아오기
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex min-h-full w-full flex-col">
       <div className="mt-[28px] flex shrink-0 items-center justify-between px-[20px]">
         <BasicTitle
           titleIconSrc="/icons/checklist-icon.svg"
@@ -16,7 +16,7 @@ export default function CategoryEditPage() {
       </div>
       <div className="flex-1 overflow-y-auto">
         <FriendListFrame isEmpty={false} content="카테고리가 없습니다.">
-          <div className="flex flex-col gap-[7px] px-5 pt-6">
+          <div className="flex flex-col gap-[7px] px-5">
             <CategoryList initialCategories={INITIAL_CATEGORY} />
           </div>
         </FriendListFrame>
