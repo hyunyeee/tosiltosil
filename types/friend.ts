283a55554745ebdec11c6f2754587b6c63b1b313
hyunyeee <5004ohy@naver.com>
2025-07-28 01:24:52 +0900

    refactor: FriendHomeHeaderRelationship 타입 추가 #32

--- types/friend.ts
+++ types/friend.ts
@@ -12,3 +12,9 @@ export type FriendResult = BaseUserInfo;
 export interface SearchResult extends BaseUserInfo {
   relationship?: "FRIEND" | "PENDING";
 }
+
+export type FriendHomeHeaderRelationship =
+  | "REQUEST_RECEIVED"
+  | "REQUEST_SENT"
+  | "FRIEND"
+  | "GENERAL";
