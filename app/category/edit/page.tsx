c5b9c07ca23beb75db65b8ca27cba19b2467fe83
kimdongyoung <wwweric12@naver.com>
2025-07-30 15:07:18 +0900

    refactor: CategoryEdit page 컴포넌트 분리 #39 - ssr을 활용을 위해 컴포넌트 분리

--- app/category/edit/page.tsx
+++ app/category/edit/page.tsx
@@ -1,36 +1,11 @@
-"use client";
-import { useGetCategories } from "@/apis/category/queries";
-import CategoryList from "@/components/category/CategoryList";
-import BasicTitle from "@/components/friend/BasicTitle";
-import FriendListFrame from "@/components/friend/FriendListFrame";
+import CategoryContainer from "@/components/category/edit/CategoryContainer";
 
 export default function CategoryEditPage() {
   // TODO: 카테고리 받아오기
-  const { data: categories, isLoading, error } = useGetCategories();
-  if (isLoading) {
-    return <div></div>;
-  }
+
   return (
     <div className="flex min-h-full w-full flex-col">
-      <div className="mt-[28px] flex shrink-0 items-center justify-between px-[20px]">
-        <BasicTitle
-          titleIconSrc="/icons/checklist-icon.svg"
-          text="카테고리"
-          buttonIconType="plus"
-        />
-      </div>
-      <div className="flex-1 overflow-y-auto">
-        <FriendListFrame
-          isEmpty={!categories || categories.length === 0}
-          content="카테고리가 없습니다."
-        >
-          {categories && (
-            <div className="flex flex-col gap-[7px] px-5">
-              <CategoryList initialCategories={categories} />
-            </div>
-          )}
-        </FriendListFrame>
-      </div>
+      <CategoryContainer />
     </div>
   );
 }
---
+++ components/category/edit/CategoryContainer.tsx
@@ -0,0 +1,34 @@
+"use client";
+import BasicTitle from "@/components/friend/BasicTitle";
+import FriendListFrame from "@/components/friend/FriendListFrame";
+import CategoryList from "./CategoryList";
+import { useGetCategories } from "@/apis/category/queries";
+
+const CategoryContainer = () => {
+  const { data: categories, isLoading, error } = useGetCategories();
+  return (
+    <>
+      <div className="mt-[28px] flex shrink-0 items-center justify-between px-[20px]">
+        <BasicTitle
+          titleIconSrc="/icons/checklist-icon.svg"
+          text="카테고리"
+          buttonIconType="plus"
+        />
+      </div>
+      <div className="flex-1 overflow-y-auto">
+        <FriendListFrame
+          isEmpty={!categories || categories.length === 0}
+          content="카테고리가 없습니다."
+        >
+          {categories && (
+            <div className="flex flex-col gap-[7px] px-5">
+              <CategoryList initialCategories={categories} />
+            </div>
+          )}
+        </FriendListFrame>
+      </div>
+    </>
+  );
+};
+
+export default CategoryContainer;
