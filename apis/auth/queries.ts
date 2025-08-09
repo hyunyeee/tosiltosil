7a2a887d9826bf247fe8ff3370b758d6747b03d5
hyunyeee <5004ohy@naver.com>
2025-08-09 11:43:11 +0900

    refactor: useMutation 제네릭 지정 #43

--- apis/auth/queries.ts
+++ apis/auth/queries.ts
@@ -1,10 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { login, sendEmail, signup, verifyCode } from "@/apis/auth/api";
+import {
+  LoginPayload,
+  SendEmailPayload,
+  VerifyCodePayload,
+} from "@/apis/auth/types";
+import { ApiResponse } from "@/types/api/api";
 
 export const useLogin = () => {
   const router = useRouter();
-  return useMutation({
+  return useMutation<ApiResponse<{ memberId: string }>, Error, LoginPayload>({
     mutationFn: login,
     onSuccess: (data) => {
       console.log("로그인 성공", data);
@@ -18,7 +24,7 @@ export const useLogin = () => {
 
 export const useSignup = () => {
   const router = useRouter();
-  return useMutation({
+  return useMutation<ApiResponse<{ nickname: string }>, Error, FormData>({
     mutationFn: signup,
     onSuccess: (data) => {
       console.log("회원가입 성공", data);
@@ -31,7 +37,7 @@ export const useSignup = () => {
 };
 
 export const useSendEmail = () => {
-  return useMutation({
+  return useMutation<ApiResponse<{ email: string }>, Error, SendEmailPayload>({
     mutationFn: sendEmail,
     onSuccess: (data) => {
       console.log("인증코드 이메일 전송 성공", data);
@@ -43,7 +49,7 @@ export const useSendEmail = () => {
 };
 
 export const useVerifyCode = () => {
-  return useMutation({
+  return useMutation<ApiResponse<void>, Error, VerifyCodePayload>({
     mutationFn: verifyCode,
   });
 };
