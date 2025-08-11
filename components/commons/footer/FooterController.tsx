14ff2daff0464be0cf465612cc36b0d7cf311108
kimdongyoung <wwweric12@naver.com>
2025-08-11 16:00:10 +0900

    fix: footer 및 navbar를 독립적으로 구분 #41

--- components/commons/footer/FooterController.tsx
+++ components/commons/footer/FooterController.tsx
@@ -8,26 +8,19 @@ import PolicyFooter from "./PolicyFooter/PolicyFooter";
 const FooterController = () => {
   const pathname = usePathname();
 
-  const getFooterType = () => {
-    if (FOOTER_CONFIG.nav.some((path) => pathname.startsWith(path))) {
-      return "nav";
-    }
-    if (FOOTER_CONFIG.policy.some((path) => pathname.startsWith(path))) {
-      return "policy";
-    }
-    return "none";
-  };
+  const showNavBar = FOOTER_CONFIG.nav.some((path) =>
+    pathname.startsWith(path)
+  );
+  const showPolicyFooter = FOOTER_CONFIG.policy.some((path) =>
+    pathname.startsWith(path)
+  );
 
-  const footerType = getFooterType();
-
-  switch (footerType) {
-    case "nav":
-      return <BottomNavBar />;
-    case "policy":
-      return <PolicyFooter />;
-    default:
-      return null;
-  }
+  return (
+    <>
+      {showPolicyFooter && <PolicyFooter />}
+      {showNavBar && <BottomNavBar />}
+    </>
+  );
 };
 
 export default FooterController;
