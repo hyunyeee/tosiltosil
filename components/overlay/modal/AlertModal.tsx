6074db3d5a184d6e3ba74a10ae3cdf97441db731
hyunyeee <5004ohy@naver.com>
2025-08-21 02:30:23 +0900

    refactor: title, content 를 prop 으로 받아서 사용하도록 변경 #43

--- components/overlay/modal/AlertModal.tsx
+++ components/overlay/modal/AlertModal.tsx
@@ -1,16 +1,22 @@
 import { useOverlay } from "@/hooks/useOverlay";
 import PrimaryButton from "@/components/commons/button/PrimaryButton";
 
-const AlertModal = () => {
+interface AlertModalProps {
+  title: string;
+  content: string;
+}
+const AlertModal = ({ title, content }: AlertModalProps) => {
   const { closeOverlay } = useOverlay();
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center justify-center">
       <img src="/images/modal-vector.svg" />
       <img src="/icons/alert-circle-icon.svg" className="mt-[23px] mb-[7px]" />
-      <div className="body1 text-primary-mainText">title</div>
+      <div className="body1 text-primary-mainText">{title}</div>
       <div className="bg-primary-gray my-[13px] h-[1px] w-full" />
-      <p className="text-primary-mainText footnote mb-[24px]">content</p>
+      <p className="text-primary-mainText footnote mb-[24px] text-center whitespace-pre-wrap">
+        {content}
+      </p>
       <PrimaryButton
         size="sub"
         text="확인"
