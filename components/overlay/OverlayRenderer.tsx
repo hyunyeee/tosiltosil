3293aab7e1311aac03e3797c88ad77edd9bc5510
hyunyeee <5004ohy@naver.com>
2025-08-22 01:03:33 +0900

    feat: 모달 바깥 배경을 클릭하는 경우에도 모달을 닫는 기능 구현 #43

--- components/overlay/OverlayRenderer.tsx
+++ components/overlay/OverlayRenderer.tsx
@@ -5,20 +5,35 @@ import { overlayAtom } from "@/atoms/overlayAtom";
 import Portal from "@/components/overlay/Portal";
 import ModalContainer from "@/components/overlay/ModalContainer";
 import BottomSheetContainer from "@/components/overlay/BottomSheetContainer";
+import { useOverlay } from "@/hooks/useOverlay";
 
 export default function OverlayRenderer() {
   const [overlay] = useAtom(overlayAtom);
+  const { closeOverlay } = useOverlay();
+
+  const handleBackdropClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (e.target === e.currentTarget) {
+      closeOverlay();
+    }
+  };
 
   if (!overlay.content) return null;
 
   return (
     <Portal>
-      <div className="fixed h-full min-h-screen w-full max-w-[430px] bg-black/20">
+      <div
+        onClick={handleBackdropClick}
+        className="fixed h-full min-h-screen w-full max-w-[430px] bg-black/20"
+      >
         {overlay.type === "modal" && (
-          <ModalContainer>{overlay.content}</ModalContainer>
+          <div onClick={(e) => e.stopPropagation()}>
+            <ModalContainer>{overlay.content}</ModalContainer>
+          </div>
         )}
         {overlay.type === "bottomSheet" && (
-          <BottomSheetContainer>{overlay.content}</BottomSheetContainer>
+          <div onClick={(e) => e.stopPropagation()}>
+            <BottomSheetContainer>{overlay.content}</BottomSheetContainer>
+          </div>
         )}
       </div>
     </Portal>
